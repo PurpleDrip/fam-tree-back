316868bd2c99816332bd8bc89446943e95f90bb2
PurpleDrip <gurupreethnagesha@gmail.com>
2025-03-03 09:43:15 +0530

    Using chalk package, added a custom logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,17 @@
+import chalk from "chalk";
+
+export const alert = (msg: string) => {
+    console.log(chalk.bgRed(msg));
+}
+
+export const status = (msg: string) => {
+    console.log(chalk.bgYellow(msg));
+}
+
+export const success = (msg: string) => {
+    console.log(chalk.bgGreen(msg));
+}
+
+export const info = (msg: string) => {
+    console.log(chalk.bgBlue(msg));
+}
