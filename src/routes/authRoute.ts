7884ecb3c5d35e371167c7d258a03649995e1752
PurpleDrip <gurupreethnagesha@gmail.com>
2025-03-03 12:46:45 +0530

    Added route for checking cookies

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -2,11 +2,13 @@ import e from "express"
 
 import { registerUser,loginUser, clearCookies } from "../controllers/authController";
 import { setCookie } from "../utils/setCookie";
+import { CheckForCookies } from "../middlewares/authMiddleware";
 
 const router=e.Router();
 
 router.post("/register",registerUser,setCookie);
 router.post("/login",loginUser,setCookie)
 router.post("/logout",clearCookies)
+router.get("/session",CheckForCookies)
 
 export default router;
