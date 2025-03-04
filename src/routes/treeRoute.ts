cfe274f2e221263e8e9091003bcce2cf04821598
PurpleDrip <gurupreethnagesha@gmail.com>
2025-03-04 07:16:36 +0530

    Added route for fetching tree by their names

--- src/routes/treeRoute.ts
+++ src/routes/treeRoute.ts
@@ -1,6 +1,6 @@
 import e from "express";
 
-import { addEdge, createTree, getTree, getTreeById, updateEdge } from "../controllers/treeController";
+import { addEdge, createTree, getTree, getTreeById, getTreeByName, updateEdge } from "../controllers/treeController";
 import {validateCookie, validateUser} from "../middlewares/validateMiddleware"
 import { UpdateCache } from "../middlewares/cacheMiddleware";
 
@@ -9,6 +9,7 @@ const router=e.Router();
 router.post("/addtree",validateUser,createTree,UpdateCache)
 router.get("/tree",validateUser,getTree)
 router.get("/tree/:id",validateCookie,getTreeById)
+router.get("/tree/:treeName",validateCookie,getTreeByName)
 
 router.post("/addedge",validateUser,addEdge,UpdateCache)
 router.post("/updateedge",validateUser,updateEdge,UpdateCache)
