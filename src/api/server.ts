543bf6cde5e8b63cc7b58b71aa021aae6e175b2c
PurpleDrip <gurupreethnagesha@gmail.com>
2025-03-09 15:03:11 +0530

    Remove vercel node

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,20 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express, { Request, Response, NextFunction } from "express";
+import express,{ Request, Response, NextFunction }  from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { inject } from "@vercel/analytics";
-import { VercelRequest, VercelResponse } from "@vercel/node";
 
 import { alert, info, success } from "../utils/logger";
 import connectDB from "../config/db";
-import authRoute from "../routes/authRoute";
-import treeRoute from "../routes/treeRoute";
-import nodeRoute from "../routes/nodeRoute";
+import authRoute from "../routes/authRoute"
+import treeRoute from "../routes/treeRoute"
+import nodeRoute from "../routes/nodeRoute"
 
 const app = express();
-const FRONTEND_URL = process.env.FRONTEND_URL;
+const PORT = process.env.PORT || 5000;
+const FRONTEND_URL=process.env.FRONTEND_URL;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,29 +22,33 @@ app.use(cookieParser());
 app.use(cors({
     origin: FRONTEND_URL, 
     credentials: true, 
-}));
+  }));
 
 app.use((req, res, next) => {
-    inject();
-    next();
+      inject();
+      next();
 });
 
 app.get("/", (req, res) => {
     res.send("Official RestAPI for famtree.in");
 });
 
-export default async (req: VercelRequest, res: VercelResponse) => {
-    await connectDB();  
-    info("Successfully connected to MongoDB")
-    return app(req, res);
-};
+app.use("/api/auth",authRoute);
+app.use("/api",treeRoute);
+app.use("/api/node",nodeRoute);
 
-app.use("/api/auth", authRoute);
-app.use("/api", treeRoute);
-app.use("/api/node", nodeRoute);
-
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+app.use((err:any, req: Request, res:Response, next:NextFunction) => {
     console.error("Global Error Handler:", err);
-    alert(err)
     res.status(500).json({ success: false, message: "Internal Server Error" });
+  });
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        success(`✅ Server is running on port ${PORT}`);
+        info(`RestAPI running at "http://localhost:${PORT}"\n`)
+    });
+}).catch(err => {
+    alert("❌ Server startup failed due to DB connection issue:"+ err);
+    process.exit(1);
 });
