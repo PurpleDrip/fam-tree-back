d67037f2dee7f8785ffddc25d3075d608678d4dd
PurpleDrip <gurupreethnagesha@gmail.com>
2025-03-16 23:51:46 +0530

    updated user model to login using treename and password

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,38 +1,15 @@
 import mongoose, { Schema, Types } from 'mongoose';
 
 interface IUser extends Document{
-    mode:string,
-    email:string,
-    username:string,
-    gender:string,
-    dob:string,
+    adminPassword:string,
     password:string,
     treeName:string,
     treeId:Types.ObjectId
 }
 
 const UserSchema = new Schema<IUser>({
-    mode:{
+    adminPassword:{
         type:String,
-        required:true,
-        enum:["google","default","facebook"]
-    },
-    email:{
-        type:String,
-    },
-    username: { 
-        type: String, 
-        required: true,
-        unique: true
-    },
-    gender: { 
-        type: String, 
-        enum: ["male", "female", "others"], 
-        required: true 
-    },
-    dob: { 
-        type: String, 
-        required: true 
     },
     password: { 
         type: String, 
