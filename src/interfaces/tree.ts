c389e4c15b7b1bf75a6e11e9a5dee9113ffcafe3
PurpleDrip <gurupreethnagesha@gmail.com>
2025-03-17 23:27:14 +0530

    added interface for redis Data

---
+++ src/interfaces/tree.ts
@@ -0,0 +1,8 @@
+import { INode } from "../models/nodeModel";
+import { IEdge } from "../models/treeModel";
+
+export default interface redisTree {
+    treeName:string,
+    nodes:Array<INode>,
+    edges:Array<IEdge>,
+}
